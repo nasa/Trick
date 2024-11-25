7152e92d8b1f9bd66b85f54dda1f909c546f7238
Patrick Herrington <pherring04@gmail.com>
2024-11-24 22:03:22 -0600

    Checkpoint

--- test/SIM_trickified/trickified_project/testing/testing.cpp
+++
@@ -1,3 +0,0 @@
-#include "testing.hh"
-
-test_class::test_class() : a(1), b(2), c(3) { print();print();print();print();print(); }
--- test/SIM_trickified/trickified_project/trickified/myproject_vars.mk
+++ test/SIM_trickified/trickified_project/trickified/myproject_vars.mk
@@ -4,15 +4,14 @@
 export MYPROJECT_HOME := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 # Specify include paths for your headers.
-MYPROJECT_INCLUDE := -I$(MYPROJECT_HOME)/include
+MYPROJECT_INCLUDE := -I$(MYPROJECT_HOME)/include_bar -I$(MYPROJECT_HOME)/include_foo
 
 # Users may set different flags for C and C++, so you should really modify both
 # to be safe.
 TRICK_CFLAGS   += $(MYPROJECT_INCLUDE) $(MYPROJECT_SOURCE)
 TRICK_CXXFLAGS += $(MYPROJECT_INCLUDE) $(MYPROJECT_SOURCE)
 
-export TRICKIFY_BUILD_TYPE := STATIC
-export TRICKIFY_OBJECT_NAME := trickified_myproject.a
+export TRICKIFY_OBJECT_NAME := trickified_myproject.o
 MYPROJECT_TRICK := $(MYPROJECT_HOME)/trickified/$(TRICKIFY_OBJECT_NAME)
 
 # Tell Trick the headers and source at this location are part of a
